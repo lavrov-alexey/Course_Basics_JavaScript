884a682405699c197c076e375c5b777ff5ab2c75
lavrov-alexey <lavrov-box@mail.ru>
2023-01-17 08:43:22 +0300

    mornings progress-2

--- lesson-8/cart.js
+++ lesson-8/cart.js
@@ -1,6 +1,14 @@
 'use strict';
 
-const cartEl = document.querySelector('span.cartIconWrap');
-//счетчик товаров в корзине
-let cartCount = cartEl.lastElementChild;  
+const cartEl = document.querySelector('span.cartIconWrap');  // корзина
+const cartCount = cartEl.lastElementChild;  // счетчик товаров в корзине
+// контейнер с товарами
+const feturedCont = document.querySelector('div.featuredItems');
+let feturedEls = feturedCont.children;  // массив товаров на текущей странице
+// console.log(feturedCont);
+// console.log(feturedEls);
 
+//вешаем обработчик клика на контейнер с товарами
+feturedCont.addEventListener('click', ev => {
+    
+});
