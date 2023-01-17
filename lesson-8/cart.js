63d60b26aade51790cee092656bc0f542a2c01d5
lavrov-alexey <lavrov-box@mail.ru>
2023-01-17 22:13:41 +0300

    evening progress

--- lesson-8/cart.js
+++ lesson-8/cart.js
@@ -1,14 +1,27 @@
 'use strict';
 
-const cartEl = document.querySelector('span.cartIconWrap');  // корзина
-const cartCount = cartEl.lastElementChild;  // счетчик товаров в корзине
+const cartWindowEl = document.querySelector('.cart');  // окно корзины
+// счетчик над корзиной - ищем сначала эл-т с классом cartIconWrap и в нем span
+const cartCountEl = document.querySelector('.cartIconWrap span');
+// при загрузке страницы - счетчик изначально обнуляем
+cartCountEl.textContent = '0';
+const cartObj = {};
+
 // контейнер с товарами
 const feturedCont = document.querySelector('div.featuredItems');
 let feturedEls = feturedCont.children;  // массив товаров на текущей странице
-// console.log(feturedCont);
-// console.log(feturedEls);
+
+// вешаем обработчик на клик по иконке корзины
+document.querySelector('span.cartIconWrap').addEventListener('click', ev => {
+    cartWindowEl.classList.toggle('hidden');
+});
+
 
 //вешаем обработчик клика на контейнер с товарами
 feturedCont.addEventListener('click', ev => {
-    
+    // если клик не на кнопке в товаре - ничего не делаем
+    if (!ev.target.classList.contains('addToCart')) return;
+    // увеличиваем счетчик на корзине
+    cartCountEl.textContent = +cartCountEl.textContent + 1;
+
 });
